c847ab376ef9bffe0bd2bee5b2278398f6fa7541
Jengza45 <namnueng.int@damrong.ac.th>
2025-08-21 00:20:18 +0700

    ข้อความ

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App..module.css";
+import img from "./images/drawers.jpg";
+
+// import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.wrapper}>
+      <div className={styles.container_main}>
+        {" "}
+        <img src={img} alt="" />
+        <div>
+          <h1 style={{ padding: "20px" }}>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h1>
+          <p>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I’ve got some simple tips to help
+            you make any room feel complete. Michelle Appleton 28 Jun 2020 Share
+          </p>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
